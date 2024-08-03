6fd45ce658f235dd9cd71dacfa961107beabe486
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-08-03 16:53:08 -0700

    Test: Add test for char count

--- main_test.go
+++ main_test.go
@@ -56,3 +56,19 @@ func TestCounterOutputsCorrectNumberOfWords(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func TestCounterOutputsCorrectNumberOfChars(t *testing.T) {
+	cmd := exec.Command("wc", "-m", "test.txt")
+	output, err := cmd.Output()
+	if err != nil {
+		t.Fatal("Ran into an error running `wc -m test.txt`:", err.Error())
+	}
+	got := counter("test.txt", bufio.ScanRunes)
+	want, err := strconv.Atoi(strings.Split(string(output[:]), " ")[0])
+	if err != nil {
+		t.Fatal("Ran into an error parsing the numbers from `wc`'s output:", err.Error())
+	}
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
