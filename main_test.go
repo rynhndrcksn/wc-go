0a2e16681a0ce5b88492bd6ec4685dcdea1d85cf
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2024-08-03 16:48:37 -0700

    Test: Add test for number of words

--- main_test.go
+++ main_test.go
@@ -40,3 +40,19 @@ func TestCounterOutputsCorrectNumberOfLines(t *testing.T) {
 		t.Errorf("got %d, wanted %d", got, want)
 	}
 }
+
+func TestCounterOutputsCorrectNumberOfWords(t *testing.T) {
+	cmd := exec.Command("wc", "-w", "test.txt")
+	output, err := cmd.Output()
+	if err != nil {
+		t.Fatal("Ran into an error running `wc -w test.txt`:", err.Error())
+	}
+	got := counter("test.txt", bufio.ScanWords)
+	want, err := strconv.Atoi(strings.Split(string(output[:]), " ")[0])
+	if err != nil {
+		t.Fatal("Ran into an error parsing the numbers from `wc`'s output:", err.Error())
+	}
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
